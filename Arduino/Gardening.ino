e699a4f985238f21a5f3b238849eedf161db5660
Mika Ristimaki <mika.ristimaki@vertoanalytics.com>
2014-07-04 01:13:30 +0300

    Adding button and led, fixing delay value

--- Arduino/Gardening.ino
+++ Arduino/Gardening.ino
@@ -6,48 +6,66 @@ MoistureSensor moistureSensor2(6, 7, A4, 200, 0.10);
 MoistureSensor moistureSensor3(8, 9, A3, 200, 0.10);
 
 TemperatureSensor temperatureSensor1(10, A1);
-TemperatureSensor temperatureSensor2(12, A0);
+TemperatureSensor temperatureSensor2(11, A0);
+
+const unsigned long FLAG_CHECK_INTERVAL = 10000;
+const unsigned long HOUR = 86400000;
+const int LED_PIN = 12;
+
+boolean isFirst = true;
+volatile boolean buttonPressed = false;
 
 void setup() {
   Serial.begin(9600);
-  for (int i = 0; i < 13; i++) {
+  for (int i = 0; i <= 13; i++) {
     	digitalWrite(i, LOW);
-        //pinMode(i, OUTPUT);
   }
   
-  digitalWrite(3,HIGH);
+  pinMode(3,OUTPUT);
+  digitalWrite(3,LOW);
+  pinMode(LED_PIN,OUTPUT);
+  digitalWrite(LED_PIN, LOW);
+  
+  // Wait for the router to boot
+  Serial.println("debug starting to boot the router");
+  delay(60000);
+  Serial.println("debug router booted");
+ 
+  attachInterrupt(0, buttonTriggered, FALLING);
 }
 
 void loop() {
-   Serial.println("debug Starting to read temperature");
-   float temperature1 = temperatureSensor1.readTemperature(1000);
-   Serial.print("Temperature1: ");
-   Serial.println(temperature1);
-   delay(1000);
-   
-  float temperature2 = temperatureSensor2.readTemperature(1000);
-   Serial.print("Temperature2: ");
-   Serial.println(temperature2);
-   delay(1000);
-   
-   
-  Serial.println("debug Starting to read moisture");
-   int moisture1 = moistureSensor1.readMoisture(3000);
-  // Serial.print("Moisture1: ");
-   //Serial.println(moisture1);
-   delay(1000);
-   
-   int moisture2 = moistureSensor2.readMoisture(3000);
-  // Serial.print("Moisture2: ");
-  // Serial.println(moisture2);
-   delay(1000);
+  
+  digitalWrite(LED_PIN, HIGH);
+  if (!isFirst) {
+     Serial.println("/root/network-up");
+     delay(30000);
+  }
+   isFirst = false;
 
-   int moisture3 = moistureSensor3.readMoisture(3000);
-  // Serial.print("Moisture3: ");
-  // Serial.println(moisture3);
+   Serial.println("debug Starting to read temperature");
+   float temperature1 = temperatureSensor1.readTemperature(2000);
+   delay(500);
    
-   sendMeasurements(temperature1, temperature2, moisture1, moisture2, moisture3);
-   delay(30*1000);
+   float temperature2 = temperatureSensor2.readTemperature(2000);
+   delay(500);
+   
+   Serial.println("debug Starting to read moisture");
+   int moisture1 = moistureSensor1.readMoisture(2000);
+   delay(500);
+   
+   int moisture2 = moistureSensor2.readMoisture(2000);
+   delay(500);
+
+   int moisture3 = moistureSensor3.readMoisture(2000);
+   
+   sendMeasurements(temperature1, temperature2, moisture1, moisture2, moisture3);   
+  
+   Serial.println("/root/network-down");
+   digitalWrite(LED_PIN, LOW);
+
+   Serial.println("debug starting to wait");
+   waitLong(HOUR);
 }
 
 void sendMeasurements(float t1, float t2, int m1, int m2, int m3) {
@@ -70,4 +88,22 @@ void print(String param) {
   Serial.print("&");
   Serial.print(param);
   Serial.print("=");
+}
+
+void waitLong(unsigned long ms) {
+  digitalWrite(3, HIGH);
+  // We wait so the interreupt has time to run
+  delay(100);
+  buttonPressed = false;
+  unsigned long parts = ms / FLAG_CHECK_INTERVAL;  
+  for (unsigned int i = 0; i < parts && !buttonPressed; i++) {
+     delay(FLAG_CHECK_INTERVAL);
+  }
+  digitalWrite(3, LOW);
+  delay(100);
+  buttonPressed = false;  
+}
+
+void buttonTriggered() {
+  buttonPressed = true; 
 }
