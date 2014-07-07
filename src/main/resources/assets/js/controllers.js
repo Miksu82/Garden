ab8e343ed85543940f67297379e89efbe3fae349
Mika Ristimaki <mika.ristimaki@vertoanalytics.com>
2014-07-07 19:38:21 +0300

    Adding angular and other javascript

---
+++ src/main/resources/assets/js/controllers.js
@@ -0,0 +1,78 @@
+'use strict';
+
+/* Controllers */
+
+var gardenApp = angular.module('gardenApp', ["highcharts-ng"]);
+
+gardenApp.controller('GardenDataAll', function($scope, $http) {
+	$http.get('../garden/query/?hid=1').success(function(data) {
+		$scope.hid = data.hid;
+		$scope.response = data;
+
+		var map = Array.prototype.map;
+
+		var temperature1 = map.call(data.measurements, function(item) { 
+			var dateParts = item.date.split("-");
+			var date = Date.UTC(dateParts[0], dateParts[1], dateParts[2], item.hour);
+			return [date, item.temperature1]; 
+		});
+		var temperature2 = map.call(data.measurements, function(item) { 
+			var dateParts = item.date.split("-");
+			var date = Date.UTC(dateParts[0], dateParts[1], dateParts[2], item.hour);
+			return [date, item.temperature2]; 
+		});
+		
+		temperature1.sort(function(a,b) {
+			return a[0] - b[0];
+		});
+		temperature2.sort(function(a,b) {
+			return a[0] - b[0];
+		});
+
+		$scope.temperature1Label = data.temperature1Label;
+		$scope.temperature2Label = data.temperature2Label;
+
+		$scope.temperature1Measurements = temperature1;
+		$scope.temperature2Measurements = temperature2;	
+
+		$scope.temperatureSeries = [{"name": data.temperature1Label, "data": temperature1},
+		                            {"name": data.temperature2Label, "data": temperature2}];
+		
+		/*$scope.temperatureSeries = [{"name": data.temperature1Label, "data": [
+		                                                                      [Date.UTC(1986,2,6),1],
+		                                                                      [Date.UTC(1986,2,7),2]
+		                                                                      ]},
+		                            {"name": data.temperature2Label, "data": [
+		                                                                      [Date.UTC(1986,2,6),7],
+		                                                                      [Date.UTC(1986,2,7),6]
+		                                                                      ]}];
+*/
+		$scope.temperatureChart = {
+				options: {
+					chart: {
+						type: 'spline'
+					}
+				},
+				plotOptions: {
+					series: {
+						stacking: ""
+					}
+				},
+				series: $scope.temperatureSeries,
+				title: {
+					text: data.householdLabel
+				},
+				xAxis: {
+					type: 'datetime',
+					dateTimeLabelFormats: { // don't display the dummy year
+						month: '%e. %b',
+						year: '%b'
+					},
+					title: {
+						text: 'Date'
+					}
+				}
+		}
+
+	});
+});
